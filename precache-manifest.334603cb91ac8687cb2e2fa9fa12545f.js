a7fc9dd3e94a6f94eda464879caba6ca2c3f3171
vahedi <vahedi.ce@gmail.com>
2020-10-14 17:28:13 +0330

    Updates

---
+++ precache-manifest.334603cb91ac8687cb2e2fa9fa12545f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f016ff50533d26cd975bbb954c495030",
+    "url": "/lazy-images/index.html"
+  },
+  {
+    "revision": "3bfa0fb87764ad21753c",
+    "url": "/lazy-images/static/css/main.6a100573.chunk.css"
+  },
+  {
+    "revision": "478b67e44210f0c00bd8",
+    "url": "/lazy-images/static/js/2.40b21ab4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lazy-images/static/js/2.40b21ab4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bfa0fb87764ad21753c",
+    "url": "/lazy-images/static/js/main.cb504bc7.chunk.js"
+  },
+  {
+    "revision": "a16cee9f93d76c110920",
+    "url": "/lazy-images/static/js/runtime-main.1b8f90e4.js"
+  }
+]);
